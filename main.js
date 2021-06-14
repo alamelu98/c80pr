42f31d60a756ad0917d12704f02837010e09e5ca
alamelu98 <63118650+alamelu98@users.noreply.github.com>
2021-06-14 16:53:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,45 @@
-var ar1=[];
-function submit(){
-    
-    ar1.push(document.getElementById("name1").value);
-    document.getElementById("list").innerHTML=ar1;
+var canvas=document.getElementById("mycan1");
+var ctx=canvas.getContext("2d");
+console.log(ctx);
+ctx.fillStyle="white";
+ctx.strokeStyle="black";
+ctx.beginPath();
+ctx.fillRect(197,142,450,300);
+ctx.stroke();
 
-}
-function sort(){
- ar1.sort();
- var ar2=[];
- for(var k=0;k<ar1.length;k++){
-    ar2.push("<h4>"+ar1[k]+"</h4>");
- }
- document.getElementById("list_unsort").innerHTML=ar2.join(" ");
-}
-function search(){
-    var s=document.getElementById("name2").value;
-    console.log(s);
-    for(var i=0;i<ar1.length;i++){
-             if(s == ar1[i]){
-                 console.log(ar1[i]);
-                 var k="found "+ar1[i];
-                document.getElementById("search").innerHTML=k;
-             }
-            
-    }
-}
+ctx.beginPath();
+ctx.strokeStyle="blue";
+ctx.lineWidth=4;
+ctx.fillStyle="white";
+ctx.arc(300,217,40,0,Math.PI*2);
+ctx.stroke();
+ctx.fill();
+
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth=4;
+ctx.fillStyle="white";
+ctx.arc(410,217,40,0,Math.PI*2);
+ctx.stroke();
+ctx.fill();
+
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidth=4;
+ctx.fillStyle="white";
+ctx.arc(510,217,40,0,Math.PI*2);
+ctx.stroke();
+ctx.fill();
+
+ctx.beginPath();
+ctx.strokeStyle="yellow";
+ctx.lineWidth=4;
+ctx.arc(360,260,40,0,Math.PI*2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="green";
+ctx.lineWidth=4;
+ctx.arc(460,260,40,0,Math.PI*2);
+ctx.stroke();
+
