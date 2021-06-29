1abbe6a2cbad8f96ca112de4e3bc1664211bf016
alamelu98 <63118650+alamelu98@users.noreply.github.com>
2021-06-29 16:04:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,93 +1,91 @@
-var canvas=document.getElementById("canvas1");
+var canvas=document.getElementById("can1");
 var ctx=canvas.getContext("2d");
-var color="black";
-var line_width=1;
-var last_x_pos;
-var last_y_pos;
-var cur_x_pos;
-var cur_y_pos;
-//var mouseevent="empty";
-var width=screen.width;
-var new_width=width-70;
-var height=screen.height;
-var new_height=height-300;
-if(width<992)
+//width and height
+var c1_w=120;
+var c1_h=70;
+var c2_w=120;
+var c2_h=70;
+//images of cars
+var c1_img="https://i.postimg.cc/YqdnnNX1/car1.png";
+var c2_img="https://i.postimg.cc/YqdnnNX1/car1.png";
+//bg image of canvas
+var bg_img="https://image.shutterstock.com/image-vector/vector-illustration-racing-cars-go-260nw-1042498327.jpg"
+//pos
+var c1_x=10
+var c1_y=10
+var c2_x=10
+var c2_y=100
+//function add
+function add()
 {
- document.getElementById("canvas1").width=new_width;
- document.getElementById("canvas1").height=new_height;
- document.body.style.overflow="hidden";
-}
+    var c1img=new Image();
+    c1img.onload=c1im();
+    c1img.src=c1_img;
 
-   canvas.addEventListener("touchstart",touchstart)
-   function touchstart(e){
-       console.log("touchstart");
-       color=document.getElementById("clr").value;
-       line_width=document.getElementById("lw").value;
-       last_x_pos=e.touches[0].clientX-canvas.offsetLeft;
-        last_y_pos=e.touches[0].clientY-canvas.offsetTop;
-   }
-   canvas.addEventListener("touchmove",touchmove)
-   function touchmove(e)
-   {
-       console.log("touchmove");
-       cur_x_pos=e.touches[0].clientX-canvas.offsetLeft;
-       cur_y_pos=e.touches[0].clientY-canvas.offsetTop;
-       
-      
-           ctx.beginPath();
-           ctx.strokeStyle=color;
-           ctx.lineWidth=line_width;
-           console.log("last position of x and last position of y");
-           console.log("x="+last_x_pos+"y="+last_y_pos);
-           ctx.moveTo(last_x_pos,last_y_pos);
-           console.log("current position of x and current position of y");
-           console.log("x="+cur_x_pos+"y="+cur_y_pos);
-           ctx.lineTo(cur_x_pos,cur_y_pos);
-          ctx.stroke();
-          last_x_pos=cur_x_pos;
-    last_y_pos=cur_y_pos;
-   }
+    var c2img=new Image();
+    c2img.onload=c2im();
+    c2img.src=c2_img;
 
+    var bgimg=new Image();
+    bgimg.onload=bgim();
+    bgimg.src=bg_img;
 
-   canvas.addEventListener("mousedown",moudwn);
-function moudwn(e)
-{
-    color=document.getElementById("clr").value;
-    line_width=document.getElementById("lw").value;
-    mouseevent="mousedown";
-}
-canvas.addEventListener("mousemove",momove);
-function momove(e){
- cur_x_pos=e.clientX-canvas.offsetLeft;
- cur_y_pos=e.clientY-canvas.offsetTop;
- if(mouseevent=="mousedown")
+ function c1im()
  {
-     ctx.beginPath();
-     ctx.strokeStyle=color;
-     ctx.lineWidth=line_width;
-     console.log("last position of x and last position of y");
-     console.log("x="+last_x_pos+"y="+last_y_pos);
-     ctx.moveTo(last_x_pos,last_y_pos);
-     console.log("current position of x and current position of y");
-     console.log("x="+cur_x_pos+"y="+cur_y_pos);
-     ctx.lineTo(cur_x_pos,cur_y_pos);
-    ctx.stroke();
-    
-
+     ctx.drawImage(c1img,c1_x,c1_y,c1_w,c1_h)
  }
- last_x_pos=cur_x_pos;
-    last_y_pos=cur_y_pos;
+
+ function c2im()
+ {
+     ctx.drawImage(c2img,c2_x,c2_y,c2_w,c2_h)
+ }
+
+ function bgim()
+ {
+     ctx.drawImage(bgimg,10,10,canvas.width,canvas.height)
+ }
+}
+window.addEventListener("keydown",downkey)
+function downkey(e)
+{
+    var keyno=e.keyCode;
+    console.log(keyno);
+    if(keyno=="38")
+    {
+        up();
+        console.log("up");
+    }
+    if(keyno=="39")
+    {
+        right();
+        console.log("right");
+    }
+    if(keyno=="40")
+    {
+        down();
+        console.log("down");
+    }
+    if(keyno=="37")
+    {
+        left();
+        console.log("left");
+    }
+}
+function up()
+{
 
 }
-canvas.addEventListener("mouseup", my_mouseup);
-    function my_mouseup(e)
-    {
-        mouseevent = "mouseup";
-    }
+function down()
+{
+    
+}
 
-    canvas.addEventListener("mouseleave", my_mouseleave);
-    function my_mouseleave(e)
-    {
-        mouseevent = "mouseleave";
-    }
- 
+function left()
+{
+    
+}
+
+function right()
+{
+    
+}
